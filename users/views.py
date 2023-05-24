415b13700d5ba48d3e967b02198cb23f3d41bcaa
Stan Mikhaylov <mikhaylov.st@gmail.com>
2023-05-24 20:04:37 +0300

    Личный кабинет

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth
 from django.urls import reverse
 
@@ -29,4 +29,20 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Store - Профиль',
+        'form': form
+    }
+    return render(request, 'users/profile.html', context)
 
